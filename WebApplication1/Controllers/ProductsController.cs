302bb1701b3cc42c704d7d2c8f61111b840af538
RQ <RQ@BE-NBK-019>
2023-06-07 11:12:52 +0200

    Add project files.

---
+++ WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        [HttpGet]
+        [Route("api/products")]
+        public IEnumerable<Product> GetProducts()
+        {
+            // Richiama il metodo esistente GetProducts() del code-behind
+            return GetProductsFromCodeBehind();
+        }
+
+    }
+}
